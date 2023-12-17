483c1b08031a31ac4bf99452b1092eafb27ae080
Robert Szukalski <robert.szukalski@softwaredefinedautomation.io>
2023-12-17 09:54:21 +0100

    Added "Hello, World!" to test compiler.

--- src/main/java/com/fem/FiniteElementSolver.java
+++ src/main/java/com/fem/FiniteElementSolver.java
@@ -2,6 +2,7 @@ package com.fem;
 
 public class FiniteElementSolver {
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
         // Create beam mesh
         // Apply force
         // Set boundary conditions
---
+++ src/test/java/com/fem/FiniteElementSolverTest.java
@@ -0,0 +1,9 @@
+package com.fem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FiniteElementSolverTest {
+
+}
